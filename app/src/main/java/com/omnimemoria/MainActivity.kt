7d0f6fce03d1d8f59c9bf622b57f3466a5c9a6c1
Obieda <bis.2410423@eia.edu.eg>
2026-05-16 12:43:57 +0300

    Add files via upload

--- app/src/main/java/com/omnimemoria/MainActivity.kt
+++ app/src/main/java/com/omnimemoria/MainActivity.kt
@@ -3,6 +3,7 @@ package com.omnimemoria
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import com.omnimemoria.ui.navigation.AppNavGraph
 import com.omnimemoria.ui.theme.OmniMemoriaTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,9 +12,10 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             OmniMemoriaTheme {
-                AppNavGraph(isVaultUnlocked = false)
+                AppNavGraph()
             }
         }
     }
