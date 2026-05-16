0ad8e1c928d3d771615a41d6c5ed492005d4b355
Obieda <bis.2410423@eia.edu.eg>
2026-05-16 12:53:14 +0300

    Remove edge-to-edge feature and update navigation

--- app/src/main/java/com/omnimemoria/MainActivity.kt
+++ app/src/main/java/com/omnimemoria/MainActivity.kt
@@ -3,7 +3,6 @@ package com.omnimemoria
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import com.omnimemoria.ui.navigation.AppNavGraph
 import com.omnimemoria.ui.theme.OmniMemoriaTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,10 +11,9 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             OmniMemoriaTheme {
-                AppNavGraph()
+                AppNavGraph(isVaultUnlocked = false)
             }
         }
     }
