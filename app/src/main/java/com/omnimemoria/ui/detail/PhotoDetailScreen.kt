34e6f18022719a9a74ec6a728545da48f48edd54
Obieda <bis.2410423@eia.edu.eg>
2026-05-17 06:09:08 +0300

    Add clickable import to PhotoDetailScreen.kt

--- app/src/main/java/com/omnimemoria/ui/detail/PhotoDetailScreen.kt
+++ app/src/main/java/com/omnimemoria/ui/detail/PhotoDetailScreen.kt
@@ -5,6 +5,7 @@ import androidx.activity.compose.BackHandler
 import androidx.compose.animation.*
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
