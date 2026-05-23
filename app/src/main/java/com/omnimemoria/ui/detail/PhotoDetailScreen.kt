0ece7a63cacdce1980f2dffa527580be02acb702
Obieda <bis.2410423@eia.edu.eg>
2026-05-23 19:35:35 +0300

    Remove unused import for photoSharedKey

--- app/src/main/java/com/omnimemoria/ui/detail/PhotoDetailScreen.kt
+++ app/src/main/java/com/omnimemoria/ui/detail/PhotoDetailScreen.kt
@@ -35,7 +35,6 @@ import coil3.request.ImageRequest
 import com.omnimemoria.domain.model.MediaPhoto
 import com.omnimemoria.ui.LocalNavAnimatedVisibilityScope
 import com.omnimemoria.ui.LocalSharedTransitionScope
-import com.omnimemoria.ui.gallery.photoSharedKey
 import com.omnimemoria.ui.photoSharedKey
 import me.saket.telephoto.zoomable.coil3.ZoomableAsyncImage
 import java.text.SimpleDateFormat
